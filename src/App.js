79fbbd04a9c8875829310502e9e77139f8aeaa86
Avogardo <avogardo0@gmail.com>
2016-10-21 10:52:03 +0200

    0.5
    Stworzony szkielet i dodany murzyn

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TaskList from './TaskList.js';
 import './App.css';
+import Date from './Date.js';
+import Avatar from './Avatar.js';
+import AddButton from './AddButton.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{padding: '30px 30px'}}>
+          <Avatar />
+          <br />
+          <Date />
+          <br />
+          <TaskList />
+          <br />
+          <AddButton />
       </div>
     );
   }
